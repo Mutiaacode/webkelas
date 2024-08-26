bbb224aea68e020a4029c581118ccd2dcf67ce1f
Mutia Pegi Intanswari <149048644+Mutiaacode@users.noreply.github.com>
2024-08-26 17:05:15 +0800

    Update Schedule.jsx

--- src/Pages/Schedule.jsx
+++ src/Pages/Schedule.jsx
@@ -31,7 +31,7 @@ const GalleryKelas = () => {
     <div className="flex flex-col items-center p-4  min-h-screen">
       <h1 className="text-3xl font-bold text-white mb-6"></h1>
       {imageList.length === 0 ? (
-        <p className="text-white">No images found</p>
+        <p className="text-white">Loading ...</p>
       ) : (
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
           {imageList.map((url, index) => (
