fa4ca46491362e0c173ee69e3d806d0d7464675b
Mutia Pegi Intanswari <149048644+Mutiaacode@users.noreply.github.com>
2024-08-26 21:03:16 +0800

    Update BoxClassIg.jsx

--- src/components/BoxClassIg.jsx
+++ src/components/BoxClassIg.jsx
@@ -4,10 +4,11 @@ const BoxClassIg = () => {
 		<div id="BoxOldWeb">
 			<a href="https://ngl.link/rplb.skanthree">
 				<div className="flex justify-between">
-					<img src="/Instagram.png" alt="" className="w-auto h-6" />
+					<img src="/Instagram.svg" alt="" className="w-auto h-6" />
 					<img src="/next.png" alt="" className="h-3 w-3" />
 				</div>
 				<h1 className="text-white text-base font-semibold pr-0 mt-5">Instagram</h1>
+				<div className="text-white flex py-2 opacity-60 absolute bottom-1 text-xs">View More</div>
 			</a>
 		</div>
 	)
