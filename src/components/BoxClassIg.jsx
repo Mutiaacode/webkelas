f34b2e492457f8cf2933db489bfeb991174094f2
Mutia Pegi Intanswari <149048644+Mutiaacode@users.noreply.github.com>
2024-08-26 21:07:51 +0800

    Update BoxClassIg.jsx

--- src/components/BoxClassIg.jsx
+++ src/components/BoxClassIg.jsx
@@ -7,10 +7,8 @@ const BoxClassIg = () => {
 					<img src="/Instagram.svg" alt="" className="w-auto h-6" />
 					<img src="/next.png" alt="" className="h-3 w-3" />
 				</div>
-				<h1 className="text-white text-lg font-semibold pr-3 mt-3 absolute bottom-10">
-					Follow Instagram
-				</h1>
-				<div className="text-white flex py-2 opacity-60 absolute bottom-1 text-xs">View More</div>
+				<h1 className="text-white text-base font-semibold pr-0 mt-5">Instagram</h1>
+				
 			</a>
 		</div>
 	)
