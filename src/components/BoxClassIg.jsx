b4bb544925ccfb9b2a4d25351decbdb553086a04
Mutia Pegi Intanswari <149048644+Mutiaacode@users.noreply.github.com>
2024-08-26 21:01:28 +0800

    Update BoxClassIg.jsx

--- src/components/BoxClassIg.jsx
+++ src/components/BoxClassIg.jsx
@@ -1,18 +1,16 @@
-const BoxClassIg = () => {
+
+const BoxOldWeb = () => {
 	return (
-		<div id="BoxClassIg">
-		<a href="https://www.instagram.com/rplb.skanthree">
-				<div className="flex justify-between relative ">
-					<img src="/Instagram.svg" alt="" className="w-auto h-10" />
-					<img src="/next.png" alt="" className="h-4 w-4" />
+		<div id="BoxOldWeb">
+			<a href="https://ngl.link/rplb.skanthree">
+				<div className="flex justify-between">
+					<img src="/Instagram.png" alt="" className="w-auto h-6" />
+					<img src="/next.png" alt="" className="h-3 w-3" />
 				</div>
-				<h1 className="text-white text-lg font-semibold pr-3 mt-3 absolute bottom-10">
-				 Instagram
-				</h1>
-				<div className="text-white flex py-2 opacity-60 absolute bottom-1 text-xs">View More</div>
+				<h1 className="text-white text-base font-semibold pr-0 mt-5">Instagram</h1>
 			</a>
 		</div>
 	)
 }
 
-export default BoxClassIg
+export default BoxOldWeb
