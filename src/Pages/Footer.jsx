9ce58dfb86ed01216ff5b9250157d09d1039964a
Mutia Pegi Intanswari <149048644+Mutiaacode@users.noreply.github.com>
2024-08-26 17:02:08 +0800

    Update Footer.jsx

--- src/Pages/Footer.jsx
+++ src/Pages/Footer.jsx
@@ -21,7 +21,7 @@ const Footer = () => {
 							<ul class="text-gray-400 dark:text-gray-400 font-medium">
 								<li class="mb-2">
 									<a href="https://www.instagram.com/mtiaavv" class="hover:underline">
-										Mutia Pegi Intanswari
+										Mutia
 									</a>
 								</li>
 								<li>
